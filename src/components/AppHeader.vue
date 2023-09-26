833f6184dca95013d0af9018217921023c263e07
Elisa Boscani <elisabosc90@gmail.com>
2023-09-26 16:06:46 +0200

    creato l header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,31 @@
+<script >
+
+
+
+
+
+</script>
+
+<template>
+ <header>
+        <nav class="navbar navbar-expand-sm ">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#"><img src="../assets/img/yu-gi-oh.jpg" alt=""></a>
+            
+                <div class="collapse navbar-collapse" id="navbarID">
+                    <div class="navbar-nav">
+                        <a class="nav-link fs-3 fw-semibold " aria-current="page" href="#">Yu-Gi-Oh Api</a>
+                        
+                    </div>
+                </div>
+            </div>
+        </nav>
+  </header>
+
+</template>
+
+<style scoped>
+img{
+  width: 50px;
+}
+</style>
